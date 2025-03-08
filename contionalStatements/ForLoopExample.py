b3aaa71ec57b53315c09006d62d6b15bfc9f2906
chetanbe100x <chetanbe100x@gmail.com>
2025-03-08 19:34:16 +0530

    added some more examples

--- contionalStatements/ForLoopExample.py
+++ contionalStatements/ForLoopExample.py
@@ -1,2 +1,16 @@
 for i in range(1, 11):
     print(i)
+
+str="Chetan"
+for i in str:
+    print(i) 
+
+count=0
+while count<10:
+    print(count)
+    count+=1
+
+for i in range(0, 10):
+    if i%2==0:
+        continue
+    print(i)
